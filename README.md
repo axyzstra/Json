6f0d3d0228960d892d69c069909c063e09342259
qcloud <ubuntu@localhost.localdomain>
2024-07-15 13:21:16 +0800

    cmake 组织

--- README.md
+++
@@ -1,3 +0,0 @@
-```bash
-make # 运行
-make clean # 清理
---
+++ build/src/CMakeFiles/json.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/C++/Json-linux/lib/libjson.pdb"
+  "/home/ubuntu/C++/Json-linux/lib/libjson.so"
+  "CMakeFiles/json.dir/Error.cpp.o"
+  "CMakeFiles/json.dir/Error.cpp.o.d"
+  "CMakeFiles/json.dir/JsonElement.cpp.o"
+  "CMakeFiles/json.dir/JsonElement.cpp.o.d"
+  "CMakeFiles/json.dir/Parser.cpp.o"
+  "CMakeFiles/json.dir/Parser.cpp.o.d"
+  "CMakeFiles/json.dir/Scanner.cpp.o"
+  "CMakeFiles/json.dir/Scanner.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/json.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
